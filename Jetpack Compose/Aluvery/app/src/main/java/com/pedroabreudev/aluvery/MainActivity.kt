48b6690b564d8ccc04d07e5617cd11e9dd73a734
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2024-01-21 15:27:11 -0400

    fix: change directory name..

--- Jetpack Compose/Aluvery/app/src/main/java/com/pedroabreudev/aluvery/MainActivity.kt
+++ Jetpack Compose/Aluvery/app/src/main/java/com/pedroabreudev/aluvery/MainActivity.kt
@@ -3,10 +3,7 @@ package com.pedroabreudev.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import com.pedroabreudev.aluvery.domain.sampledata.sampleSections
 import com.pedroabreudev.aluvery.presentation.produtolist.ProductListScreen
 import com.pedroabreudev.aluvery.ui.theme.AluveryTheme
